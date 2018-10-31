a062877727e94b69645273da5f4e7d7d64be71eb
Anish Hirlekar <anish.hirlekar@metamug.com>
2018-11-01 00:00:05 +0530

    Added topic subscription code

--- src/main/java/com/metamug/pushnotifier/MainActivity.java
+++ src/main/java/com/metamug/pushnotifier/MainActivity.java
@@ -2,12 +2,20 @@ package com.metamug.pushnotifier;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.firebase.messaging.FirebaseMessaging;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TOPIC_NAME = "messages";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseMessaging.getInstance().subscribeToTopic(TOPIC_NAME);
+        Toast.makeText(this,"Subscribed to topic: "+TOPIC_NAME,Toast.LENGTH_SHORT).show();
     }
 }
